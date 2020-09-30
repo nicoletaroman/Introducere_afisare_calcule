8b2758222e5ee594f966126610cafe7e2cfdbf90
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-30 18:14:02 +0300

    Delete 10.py

--- 10.py
+++
@@ -1,5 +0,0 @@
-n=int(input("dati nr de iepuri la inceputul lunii"))
-nn=int(input("dati nr de iepuri nascuti"))
-nm=int(input("dati nr de iepuri morti"))
-nt=n-nm+nn
-print("la sfarsitul lunii sunt",nt,"iepuri")
