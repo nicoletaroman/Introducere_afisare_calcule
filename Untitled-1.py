675052d6b18c1daa36919c535ea6ceb84ef8f315
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-30 17:34:18 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+#de la tastatura se introduce nr de rand al curcubeului.De afisat denumirea culorilor.
+n=int(input("dati nr culorii"))
+if n==1:print("Rosu")
+if n==2:print("oranj")
+if n==3:print("galben")
+if n==4:print("verde")
+if n==5:print("albastru")
+if n==6:print("indigo")
+if n==7:print("violet")
+if((n>7) or (n<1)):print("curcubeul are 7 culori")
