11056fbccb4c9308b0679f476f8ad0ac01902090
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-30 18:11:44 +0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,5 @@
+n=int(input("dati nr de iepuri la inceputul lunii"))
+nn=int(input("dati nr de iepuri nascuti"))
+nm=int(input("dati nr de iepuri morti"))
+nt=n-nm+nn
+print("la sfarsitul lunii sunt",nt,"iepuri")
