22f8b095b24c6989da0f6aa1ded950be0bbf0f94
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-30 18:27:48 +0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,4 @@
+a=int(input("dati nr de oua pentru gaina 1 intr-o zi"))
+b=int(input("dati nr de oua pentru gaina 2 intr-o zi"))
+c=int(input("dati nr de oua pentru gaina 3 intr-o zi"))
+print("intr-o saptamana se obtin",(a*7)+(b*7)+(c*7),"oua")
